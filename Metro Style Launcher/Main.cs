cff32692b8a1745bb7368d2e95b6b27771d7f6a8
NekoMeow <nopeople012345@gmail.com>
2019-03-29 23:14:50 +0800

    Add MyWpnEditor To Main Project
    As Title, Add MyWpnEditor to main project, and change file names.

--- Metro Style Launcher/Main.cs
+++ Metro Style Launcher/Main.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Metro_Style_Launcher
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
--- Metro Style MyWpnEditor/Program.cs
+++ Metro Style MyWpnEditor/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Metro_Style_Launcher
+namespace Metro_Style_MyWpnEditor
 {
     static class Program
     {
